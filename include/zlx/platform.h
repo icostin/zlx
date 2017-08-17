23792bcbcfc7d709dcd6a56db2f62d278971670b
Costin Ionescu <costin.ionescu@gmail.com>
2017-08-17 06:17:39 -0700

    recognize some platforms

--- include/zlx/platform.h
+++ include/zlx/platform.h
@@ -4,5 +4,51 @@
 #include "arch.h"
 
 
+#if defined(ZLX_FREESTANDING) && ZLX_FREESTANDING
+
+#else
+
+# undef ZLX_FREESTANDING
+# define ZLX_FREESTANDING 0
+
+# if defined(_WIN32)
+
+#  define ZLX_MSWIN 1
+
+# elif defined(_AIX)
+
+#  define ZLX_AIX 1
+
+# elif defined(__unix__) || defined(__unix)
+
+#  define ZLX_UNIX 1
+
+#  include <unistd.h>
+#  if defined(POSIX_VERSION)
+#   define ZLX_POSIX POSIX_VERSION
+#  endif
+
+#  if defined(__DragonFly__)
+#   define ZLX_BSD 1
+#   define ZLX_DRAGONFLY_BSD 1
+#  elif defined(__FreeBSD__)
+#   define ZLX_BSD 1
+#   define ZLX_FREEBSD 1
+#  elif defined(__NetBSD__)
+#   define ZLX_BSD 1
+#   define ZLX_NETBSD 1
+#  elif defined(__OpenBSD__)
+#   define ZLX_BSD 1
+#   define ZLX_OPENBSD 1
+#  endif
+
+# endif /* OS defines */
+
+#endif /* ZLX_FREESTANDING */
+
+
+
+
+
 #endif /* _ZLX_PLATFORM_H */
 
