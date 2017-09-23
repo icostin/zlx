72aafb14ad31cefe412e79a6070187fc35d45a0d
Costin Ionescu <costin.ionescu@gmail.com>
2017-09-23 08:42:14 -0700

    test zlx_u8a_zcopy()

--- test/stdarray.c
+++ test/stdarray.c
@@ -6,11 +6,18 @@
 int stdarray_test (void)
 {
     uint8_t d[8];
-    static uint8_t s[] = { 0x12, 0x34, 0x56 };
+    static uint8_t s[] = { 0x12, 0x34, 0x56, 0, 0xCD };
+    static uint8_t sz[] = { 0x12, 0x34, 0x56, 0, 0xFE };
+    uint8_t * dr;
 
     memset(d, 0, sizeof(d));
-    zlx_u8a_copy(d, s, sizeof(s));
-    T(!memcmp(d, s, sizeof(s)));
+    zlx_u8a_copy(d, s, 3);
+    T(!memcmp(d, s, 3));
+
+    memset(d, 0xFE, sizeof(d));
+    dr = zlx_u8a_zcopy(d, s);
+    TE(dr == &d[3], "dr-d: %u", (int) (dr - d));
+    T(!memcmp(d, sz, sizeof(sz)));
 
     return 0;
 }
