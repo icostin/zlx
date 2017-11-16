a71c1a95b1a24ddff8bfd03fda3ed550893a9c20
Costin Ionescu <costin.ionescu@gmail.com>
2017-11-16 07:26:57 -0800

    fmt test: wip, added few more tests

--- test/fmt.c
+++ test/fmt.c
@@ -69,9 +69,44 @@ int fmt_test (void)
     TE(!strcmp((char *) buf, "test 0xFE"), "s: '%s'", (char *) buf);
 
     zlx_wbuf_init(&wb, buf, N); buf[N] = 0;
-    d = zlx_wfmt(zlx_wbuf_writer, &wb, test_width_measure, NULL, "zzz $i", 9);
+    fs = zlx_wfmt(zlx_wbuf_writer, &wb, test_width_measure, NULL, "zzz $i", 9);
     buf[wb.offset] = 0;
+    TE(fs == ZLX_FMT_WIDTH_ERROR, "fs: %u", fs);
     TE(!strcmp((char *) buf, "zzz "), "s: '%s'", (char *) buf);
+
+    d = zlx_sfmt(buf, 99, "i8=$B", (int8_t) -2);
+    TE(!strcmp((char *) buf, "i8=-2"), "s: '%s'", (char *) buf);
+
+    d = zlx_sfmt(buf, 99, "i16=$W", (int16_t) -256);
+    TE(!strcmp((char *) buf, "i16=-256"), "s: '%s'", (char *) buf);
+
+    d = zlx_sfmt(buf, 99, "u16=$w", (uint16_t) 32768);
+    TE(!strcmp((char *) buf, "u16=32768"), "s: '%s'", (char *) buf);
+
+    d = zlx_sfmt(buf, 99, "i32=$D", (int32_t) -65536);
+    TE(!strcmp((char *) buf, "i32=-65536"), "s: '%s'", (char *) buf);
+
+    d = zlx_sfmt(buf, 99, "u32=$d", (uint32_t) 3000000000);
+    TE(!strcmp((char *) buf, "u32=3000000000"), "s: '%s'", (char *) buf);
+
+    d = zlx_sfmt(buf, 99, "i64=$Q", INT64_C(-30000000000));
+    TE(!strcmp((char *) buf, "i64=-30000000000"), "s: '%s'", (char *) buf);
+
+    d = zlx_sfmt(buf, 99, "u64=$q", UINT64_C(300000000000));
+    TE(!strcmp((char *) buf, "u64=300000000000"), "s: '%s'", (char *) buf);
+
+    d = zlx_sfmt(buf, 99, "i=$I", (int) -65536);
+    TE(!strcmp((char *) buf, "i=-65536"), "s: '%s'", (char *) buf);
+
+    d = zlx_sfmt(buf, 99, "u=$i", (unsigned int) 3000000000);
+    TE(!strcmp((char *) buf, "u=3000000000"), "s: '%s'", (char *) buf);
+
+    d = zlx_sfmt(buf, 99, "l=$L", (long) -65536);
+    TE(!strcmp((char *) buf, "l=-65536"), "s: '%s'", (char *) buf);
+
+    d = zlx_sfmt(buf, 99, "ul=$l", (unsigned long) 3000000000);
+    TE(!strcmp((char *) buf, "ul=3000000000"), "s: '%s'", (char *) buf);
+
     return 0;
 }
 
