2b00a5b86e817a4604657f1129ffa7bd1b2174fe
Costin Ionescu <costin.ionescu@gmail.com>
2018-03-26 21:34:58 -0700

    tweak debug test

--- test/debug.c
+++ test/debug.c
@@ -5,8 +5,13 @@
 
 int debug_test (void)
 {
+    zlx_log_t * l = zlx_default_log;
+    zlx_default_log = &zlx_nop_log;
     ZLX_DMSG("ding");
-    ZLX_DFMT("dong\n");
+    ZLX_DMSG("dong $i", 1);
+    ZLX_DFMT("ding\n");
+    ZLX_DFMT("ding $i\n", 2);
+    zlx_default_log = l;
     return 0;
 }
 
